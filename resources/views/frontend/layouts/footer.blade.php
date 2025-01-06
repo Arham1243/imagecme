b29a27ef85dc41c58fedfc9cc61b066ecdb033ed
Arham1243 <arham404khan@gmail.com>
2025-01-07 00:10:05 +0500

    fix footer links

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -16,15 +16,20 @@
                 <div class=" footer__quickLinks">
                     <div class="title">Get in touch</div>
                     <ul>
-                        <li>
-                            <a href="tel:{{ $config['COMPANYPHONE'] ?? '' }}">
-                                <i class="bx bxs-phone"></i>
-                                {{ $config['COMPANYPHONE'] ?? '' }}</a>
-                        <li>
-                            <a href="mailto:{{ $config['COMPANYEMAIL'] ?? '' }}">
-                                <i class="bx bxs-envelope"></i>
-                                {{ $config['COMPANYEMAIL'] ?? '' }}</a>
-                        </li>
+                        @if (!empty($config['COMPANYPHONE']))
+                            <li>
+                                <a href="tel:{{ $config['COMPANYPHONE'] }}">
+                                    <i class="bx bxs-phone"></i>
+                                    {{ $config['COMPANYPHONE'] }}</a>
+                            </li>
+                        @endif
+                        @if (!empty($config['COMPANYEMAIL']))
+                            <li>
+                                <a href="mailto:{{ $config['COMPANYEMAIL'] }}">
+                                    <i class="bx bxs-envelope"></i>
+                                    {{ $config['COMPANYEMAIL'] }}</a>
+                            </li>
+                        @endif
                     </ul>
                 </div>
             </div>
@@ -32,18 +37,24 @@
         <div class="footer__copyright">
             <p>© <?= date('Y') ?> - {{ env('APP_NAME') }} . All Rights Reserved</p>
             <ul class="social-media">
-                <li>
-                    <a href="{{ sanitizedLink($config['FACEBOOK'] ?? '') }}" target="_blank"><i
-                            class="bx bxl-facebook"></i></a>
-                </li>
-                <li>
-                    <a href="{{ sanitizedLink($config['INSTAGRAM'] ?? '') }}" target="_blank"><i
-                            class="bx bxl-instagram"></i></a>
-                </li>
-                <li>
-                    <a href="{{ sanitizedLink($config['TWITTER'] ?? '') }}" target="_blank"><i
-                            class="bx bxl-twitter"></i></a>
-                </li>
+                @if (!empty($config['FACEBOOK']))
+                    <li>
+                        <a href="{{ sanitizedLink($config['FACEBOOK']) }}" target="_blank"><i
+                                class="bx bxl-facebook"></i></a>
+                    </li>
+                @endif
+                @if (!empty($config['INSTAGRAM']))
+                    <li>
+                        <a href="{{ sanitizedLink($config['INSTAGRAM']) }}" target="_blank"><i
+                                class="bx bxl-instagram"></i></a>
+                    </li>
+                @endif
+                @if (!empty($config['TWITTER']))
+                    <li>
+                        <a href="{{ sanitizedLink($config['TWITTER']) }}" target="_blank"><i
+                                class="bx bxl-twitter"></i></a>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
