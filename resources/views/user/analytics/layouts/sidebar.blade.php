5a8b51c7c110c7ca0083509fc4801e3b54079277
Arham1243 <arham404khan@gmail.com>
2025-01-08 01:31:37 +0500

    feature: add case insight cahrts

--- resources/views/user/analytics/layouts/sidebar.blade.php
+++ resources/views/user/analytics/layouts/sidebar.blade.php
@@ -6,9 +6,15 @@
     <div class="form-box__body p-0">
         <ul class="settings">
             <li class="settings-item">
-                <a href="{{ route('admin.analytics.cases') }}"
+                <a href="{{ route('user.analytics.cases') }}"
                     class="settings-item__link {{ Request::routeIs('user.analytics.cases') ? 'active' : '' }}">
-                    <i class="bx bxs-group"></i> Cases
+                    <i class="bx bx-search-alt"></i> Cases
+                </a>
+            </li>
+            <li class="settings-item">
+                <a href="{{ route('user.analytics.cases-insights') }}"
+                    class="settings-item__link {{ Request::routeIs('user.analytics.cases-insights') ? 'active' : '' }}">
+                    <i class="bx bx-bar-chart-alt"></i> Cases Insights
                 </a>
             </li>
         </ul>
