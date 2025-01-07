454b6ddbb3d410fd8760dada92c90d6c99e0d8c7
Arham1243 <arham404khan@gmail.com>
2025-01-07 23:46:21 +0500

    add likeCase in image type detail

--- resources/views/frontend/image-types/detail.blade.php
+++ resources/views/frontend/image-types/detail.blade.php
@@ -33,3 +33,38 @@
         </div>
     @endif
 @endsection
+@push('js')
+    <script>
+        const likeCase = async (likeBtn, caseId) => {
+            try {
+                const action = likeBtn.getAttribute('data-action');
+                const slug = likeBtn.getAttribute('data-slug');
+
+                const likeCaseRoute =
+                    `{{ route('frontend.cases.comments.likeCase', ['slug' => ':slug', 'action' => ':action']) }}`
+                    .replace(':slug', slug)
+                    .replace(':action', action);
+
+                const response = await axios.post(likeCaseRoute, {
+                    caseId,
+                    action
+                });
+
+                const icon = likeBtn.querySelector('i');
+                const counter = likeBtn.querySelector('.total');
+
+                if (response.data.action === 'like') {
+                    icon.className = 'bx bxs-like bx-tada';
+                    setTimeout(() => icon.classList.remove('bx-tada'), 500);
+                    likeBtn.setAttribute('data-action', 'unlike');
+                } else {
+                    icon.className = 'bx bx-like';
+                    likeBtn.setAttribute('data-action', 'like');
+                }
+                counter.textContent = response.data.likesCount;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    </script>
+@endpush
