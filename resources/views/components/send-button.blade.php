934d2b50d20da178ad358f057bab8c79726edd56
Arham1243 <arham404khan@gmail.com>
2025-01-09 22:19:48 +0500

    remove auth validation from send btn

--- resources/views/components/send-button.blade.php
+++ resources/views/components/send-button.blade.php
@@ -2,21 +2,12 @@
     $caseUrl = route('frontend.cases.comments.index', $case->slug);
     $caseTitle = $case->diagnosis_title;
 @endphp
-@if (Auth::check())
-    <button data-send-button data-popup-theme="{{ $theme }}" data-case-url="{{ $caseUrl }}"
-        data-case-title="{{ $caseTitle }}" class="{{ $class }}"
-        @if ($label) data-label="{{ $label }}" @endif type="button">
-        <i class='bx bxs-paper-plane'></i>
-        @if ($showText)
-            Send
-        @endif
-    </button>
-@else
-    <a data-label="{{ $label }}" href="{{ route('auth.login', ['redirect_url' => url()->current()]) }}"
-        class="{{ $class }}">
-        <i class='bx bxs-paper-plane'></i>
-        @if ($showText)
-            Send
-        @endif
-    </a>
-@endif
+
+<button data-send-button data-popup-theme="{{ $theme }}" data-case-url="{{ $caseUrl }}"
+    data-case-title="{{ $caseTitle }}" class="{{ $class }}"
+    @if ($label) data-label="{{ $label }}" @endif type="button">
+    <i class='bx bxs-paper-plane'></i>
+    @if ($showText)
+        Send
+    @endif
+</button>
