d51d4ce01c8adb85fd89af0d5b0c3a63932e3997
Arham1243 <arham404khan@gmail.com>
2025-01-09 20:50:41 +0500

    fix auth condition for send button

--- resources/views/components/send-button.blade.php
+++ resources/views/components/send-button.blade.php
@@ -2,11 +2,21 @@
     $caseUrl = route('frontend.cases.comments.index', $case->slug);
     $caseTitle = $case->diagnosis_title;
 @endphp
-<button data-send-button data-popup-theme="{{ $theme }}" data-case-url="{{ $caseUrl }}"
-    data-case-title="{{ $caseTitle }}" class="{{ $class }}"
-    @if ($label) data-label="{{ $label }}" @endif type="button">
-    <i class='bx bxs-paper-plane'></i>
-    @if ($showText)
-        Send
-    @endif
-</button>
+@if (Auth::check())
+    <button data-send-button data-popup-theme="{{ $theme }}" data-case-url="{{ $caseUrl }}"
+        data-case-title="{{ $caseTitle }}" class="{{ $class }}"
+        @if ($label) data-label="{{ $label }}" @endif type="button">
+        <i class='bx bxs-paper-plane'></i>
+        @if ($showText)
+            Send
+        @endif
+    </button>
+@else
+    <a data-label="{{ $label }}" href="{{ route('auth.login', ['redirect_url' => url()->current()]) }}"
+        class="{{ $class }}">
+        <i class='bx bxs-paper-plane'></i>
+        @if ($showText)
+            Send
+        @endif
+    </a>
+@endif
